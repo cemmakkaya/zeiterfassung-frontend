9e789ad5ba2e6021901652abd7edb5f95949688f
cemmakkaya <cem.akkaya@bbzbl-it.ch>
2025-05-15 13:25:52 +0200

    Stand Mittag

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,4 @@
 import { Routes } from '@angular/router';
-import { LoginComponent } from './pages/login/login.component';
-import { HomeComponent } from './pages/home/home.component';
-import { StempelnComponent } from './pages/stempeln/stempeln.component';
-import { HistoryComponent } from './pages/history/history.component';
-import { AdminComponent } from './pages/admin/admin.component';
 
 export const routes: Routes = [
   { path: '', component: LoginComponent },
